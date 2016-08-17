3365c80fc6d1e2e6595dfa9a6c0b6087d9f61691
James Homer <jameshomer85@gmail.com>
2016-08-17 15:24:14 +0100

    Fix webpack

--- showy.webpack.js
+++ showy.webpack.js
@@ -3,7 +3,7 @@ require('expose?createTexture!gl-texture2d');
 require('expose?createTransition!glsl-transition');
 require('expose?eases!eases');
 
-var Showy = require('babel?presets[]=es2015!./src/showy');
-Showy.DefaultTransitions = require('babel?presets[]=es2015!./src/transitions');
+var Showy = require('babel?presets[]=es2015!./src/showy').default;
+Showy.DefaultTransitions = require('babel?presets[]=es2015!./src/transitions').default;
 
 module.exports = Showy;
